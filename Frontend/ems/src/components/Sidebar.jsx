63079c801f69378ffc978346708c179d59bc3467
lokeshgupta1347 <guptalokesh105@gmail.com>
2026-05-27 15:06:23 +0530

    dashboard ui completed

--- Frontend/ems/src/components/Sidebar.jsx
+++ Frontend/ems/src/components/Sidebar.jsx
@@ -132,7 +132,7 @@ shadow-lg border border-white/10'>
 {/* Sidebar - mobile */}
         <aside className={`lg:hidden fixed inset-y-0 left-0 w-72
         bg-linear-to-b from-slate-900 via-slate-900 to-slate-950 text-white
-        z-50 flex flex-col transform transition-transform duration-300 ${mobileOpen ? "translate-x-0" : "translate-x-full"} `}>
+        z-50 flex flex-col transform transition-transform duration-300 ${mobileOpen ? "translate-x-0" : "-translate-x-full"} `}>
             {sidebarContent}
         </aside>
 
--- Frontend/ems/src/pages/Dashboard.jsx
+++ Frontend/ems/src/pages/Dashboard.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyAdminDashboardData, dummyEmployeeDashboardData } from '../assets/assets'
+import Loading from '../components/Loading'
+import EmployeeDashboard from '../components/EmployeeDashboard'
+import AdminDashboard from '../components/AdminDashboard'
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+  const [data,setdata]=useState(null)
+  const [loading,setloading]=useState(true)
+
+  useEffect(()=>{
+    setdata(dummyAdminDashboardData)
+    setTimeout(() => {
+      setloading(false)
+       
+    }, 1000);
+
+  },[])
+
+  if(loading)return <Loading/>
+  if(!data)return <p className='text-center text-slate-500 py-12'>Failded to load dashboard</p>
+
+  if(data.role==="ADMIN"){
+    return <AdminDashboard data={data}/>
+  }
+  else{
+    return <EmployeeDashboard data={data}/>
+  }
+  
 }
 
 export default Dashboard
