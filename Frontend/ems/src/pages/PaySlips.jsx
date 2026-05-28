50e32af7ceeca5c2ebd4f73223e1412b98a305c1
lokeshgupta1347 <guptalokesh105@gmail.com>
2026-05-28 17:20:00 +0530

    payslip page ui completed

--- Frontend/ems/src/pages/PaySlips.jsx
+++ Frontend/ems/src/pages/PaySlips.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import { useCallback, useEffect, useState } from "react"
+import { dummyEmployeeData, dummyPayslipData } from "../assets/assets";
+import Loading from "../components/Loading";
+import PaySlipList from "../components/payslip/PaySlipList";
+import GeneratePaySlipForm from "../components/payslip/GeneratePaySlipForm";
 
 const PaySlips = () => {
-  return (
-    <div>PaySlips</div>
+    const [payslips, setPayslips] = useState({})
+    const [employees, setEmployees] = useState([])
+    const [loading, setLoading] = useState(true);
+    const isAdmin = true;
+
+    const fetchPayslips = useCallback(async ()=>{
+        setPayslips(dummyPayslipData)
+        setTimeout(() => {
+            setLoading(false);
+        }, 1000);
+    },[])
+
+    useEffect(()=>{
+      fetchPayslips();
+    },[fetchPayslips])
+
+    useEffect(()=>{
+      if(isAdmin) setEmployees(dummyEmployeeData)
+    },[isAdmin])
+
+    if(loading) return <Loading></Loading>
+
+    return (
+
+      <div className="animate-fade-in">
+            <div className="flex flex-col sm:flex-row justify-between items-start 
+            sm:items-center gap-4 mb-8 ">
+                <div>
+                    <h1 className="page-title">Payslips</h1>
+                    <p className="page-subtitle">{isAdmin ? "Generate and manage employee payslips" : "Your payslip history"}</p>
+                </div>
+                {isAdmin && <GeneratePaySlipForm employees={employees} onSuccess={fetchPayslips}/>}
+            </div>
+            <PaySlipList payslips={payslips} isAdmin={isAdmin}/>
+        </div>
+    
   )
 }
 
