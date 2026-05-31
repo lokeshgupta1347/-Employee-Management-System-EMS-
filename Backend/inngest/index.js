4f3418b6fb31bb0ff14c3f865d4006833016462e
lokeshgupta1347 <guptalokesh105@gmail.com>
2026-05-31 23:31:23 +0530

    updated

--- Backend/inngest/index.js
+++ Backend/inngest/index.js
@@ -98,7 +98,7 @@ const leaveApplicationReminder = inngest.createFunction(
 // Cron: Check attendance at 11:30 AM IST (06:00 UTC) and email
 // absent employees
 const attendanceReminderCron = inngest.createFunction(
-    { id: "attendance-reminder-cron" ,triggers:[{cron: "0 30 5 * * ?"}]
+    { id: "attendance-reminder-cron" ,triggers:[{cron: "TZ=Asia/Kolkata 30 11 * * *"}]
     }, // 06:00 UTC = 11:30 AM IST
     async ({ step }) => {
         // Step 1: Get today's date range (IST)
