b2c9060d692ecf94281ea13e7068614c09606ae6
lokeshgupta1347 <guptalokesh105@gmail.com>
2026-05-31 16:27:16 +0530

    script added for creating admin

--- Backend/controllers/dashboardController.js
+++ Backend/controllers/dashboardController.js
@@ -3,6 +3,7 @@ import Attendance from "../models/Attendance.js";
 import Employee from "../models/employee.js";
 import LeaveApplication from "../models/LeaveApplication.js";
 import {DEPARTMENTS} from "../constants/departments.js"
+import Payslip from "../models/Payslip.js";
 
 // Get dashboard for employee and admin
 // GET /api/dashboard
